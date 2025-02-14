d3c8a2b1562d908dd5e5ecae557b7a22616ca7ac
Luiz Fernando Grenteski <luiz.grenteski@rrs-software.com.br>
2025-02-14 09:12:30 -0300

    :sparkles: feat: Projeto Alura.

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,18 @@
+import './Colaborador.css'
+
+const Colaborador = ({ nome, imagem, cargo, corDeFundo}) => {
+
+    const background = {backgroundColor: corDeFundo}
+
+    return (<div className='colaborador'>
+        <div className='cabecalho' style={background}>
+            <img src={imagem} alt={nome}/>
+        </div>
+        <div className='rodape'>
+            <h4>{nome}</h4>
+            <h5>{cargo}</h5>
+        </div>
+    </div>)
+}
+
+export default Colaborador
